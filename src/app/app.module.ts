7396e37a63e83bbab44a68bca39cea6af88b4bc4
sgubbala <shyam.gubbala@gmail.com>
2020-08-25 21:30:48 -0400

    Revert "Homepage auth"

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -23,19 +23,11 @@ import { authInterceptorProviders } from './helpers/auth.interceptor';
 import { FormsModule }    from '@angular/forms';
 import { ItemListComponent } from './components/item-list/item-list.component';
 import { StarComponent } from './components/shared/star/star.component';
-import { ItemComponent } from './components/item/item.component';
-import { MatDatepickerModule } from '@angular/material/datepicker';
-import { MatRadioModule } from '@angular/material/radio';
 import { AddItemComponent } from './components/add-item/add-item.component';
 import {MatDatepickerModule} from '@angular/material/datepicker';
 import {MatRadioModule} from '@angular/material/radio';
-import { FlexLayoutModule } from '@angular/flex-layout';
-import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatListModule } from '@angular/material/list';
-import { CommonModule } from '@angular/common';
-import { MatMenuModule } from '@angular/material/menu';
 import { ItemService } from './services/item.service';
-import { MatDialogModule } from '@angular/material/dialog';
+import {MatDialogModule} from '@angular/material/dialog';
 import { MaterialModule } from "./material/material.module";
 import { ReactiveFormsModule } from "@angular/forms";
 import { MatFileUploadModule } from 'mat-file-upload';
@@ -71,12 +63,7 @@ import { MatFileUploadModule } from 'mat-file-upload';
     MatGridListModule,
     MatDialogModule,
     ReactiveFormsModule,
-    MaterialModule,
-    FlexLayoutModule,
-    MatSidenavModule,
-    MatListModule,
-    MatMenuModule,
-    CommonModule
+    MaterialModule
 
   ],
   providers: [ ItemService, authInterceptorProviders ],
